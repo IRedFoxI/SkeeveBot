05a4bb48dac00c0d2830d576389fa021fefa71fc
Pim Messelink <pimmesselink@gmail.com>
2013-04-13 19:08:00 +0100

    Re-ident Mumble library

--- Kesh/Mumble/Channel.class.rb
+++ Kesh/Mumble/Channel.class.rb
@@ -5,92 +5,93 @@ module Kesh
 
 		class Channel
 
-            attr_reader :name, :description
-            attr_reader :subchannels, :root_channel, :channel_id, :linked_channels, :parent_channel
-            attr_reader :localusers
-            attr_reader :temporary, :position
+			attr_reader :name, :description
+			attr_reader :subchannels, :root_channel, :channel_id, :linked_channels, :parent_channel
+			attr_reader :localusers
+			attr_reader :temporary, :position
 
-            def initialize client, message
-                @channels = client.channels
-                @channel_id = message.channel_id
-                @root_channel = client.root_channel
-                @subchannels = []
-                @localusers = []
+			def initialize client, message
+				@channels = client.channels
+				@channel_id = message.channel_id
+				@root_channel = client.root_channel
+				@subchannels = []
+				@localusers = []
 
-                @channels[message.channel_id] = self
-                if message.has_field? :parent
-                    @parent_channel = @channels[message.parent]
-                    @parent_channel.add_subchannel self
-                end
-                @name = message.name
-            end
+				@channels[message.channel_id] = self
+				if message.has_field? :parent
+					@parent_channel = @channels[message.parent]
+					@parent_channel.add_subchannel self
+				end
+				@name = message.name
+			end
 
-            def root?
-                return @channel_id == @parent_channel.channel_id
-            end
+			def root?
+				return @channel_id == @parent_channel.channel_id
+			end
 
-            def ordered_subchannels
-                return @subchannels.sort { |ch1, ch2| ch1.position <=> ch2.position }
-            end
+			def ordered_subchannels
+				return @subchannels.sort { |ch1, ch2| ch1.position <=> ch2.position }
+			end
 
-            def tree (level = 0)
-                result = ("  "  * level) + "C " + @name + "(id:#{@channel_id},pos#{@position})\n"
-                ordered_subchannels.each { |channel| result += channel.tree(level + 1) }
-                @localusers.each { |user| result += user.tree(level + 1) }
-                return result
-            end
+			def tree (level = 0)
+				result = ("  "  * level) + "C " + @name + "(id:#{@channel_id},pos#{@position})\n"
+				ordered_subchannels.each { |channel| result += channel.tree(level + 1) }
+				@localusers.each { |user| result += user.tree(level + 1) }
+				return result
+			end
 
-            def path
-                if @parent_channel
-                    return @parent_channel.path() + "/" + @name
-                else
-                    return @name
-                end
-            end
+			def path
+				if @parent_channel
+					return @parent_channel.path() + "/" + @name
+				else
+					return @name
+				end
+			end
 
-            def update client, message
-                if message.has_field? :temporary
-                    @temporary = message.temporary
-                end
+			def update client, message
+				if message.has_field? :temporary
+					@temporary = message.temporary
+				end
 
-                if message.has_field? :position
-                    @position = message.position
-                end
+				if message.has_field? :position
+					@position = message.position
+				end
 
 
-                if message.has_field? :description
-                    @description = message.description
-                end
+				if message.has_field? :description
+					@description = message.description
+				end
 
-                if (message.has_field? :description_hash) and  (@description_hash != message.description_hash)
-                    @description_hash = message.description_hash
-                    client.send_request_blob nil, nil, @channel_id
-                end
-            end
+				if (message.has_field? :description_hash) and  (@description_hash != message.description_hash)
+					@description_hash = message.description_hash
+					client.send_request_blob nil, nil, @channel_id
+				end
+			end
 
-            def remove
-                @parent_channel.subchannels.delete self
-                @channels.delete @channel_id
-            end
+			def remove
+				@parent_channel.subchannels.delete self
+				@channels.delete @channel_id
+			end
 
-            def add_localuser user
-                @localusers << user
-            end
+			def add_localuser user
+				@localusers << user
+			end
 
-            def remove_localuser user
-                @localusers.delete user
-            end
+			def remove_localuser user
+				@localusers.delete user
+			end
 
-            def inspect
-                return name
-            end
+			def inspect
+				return name
+			end
 
-            protected
-            def add_subchannel subchannel
-                @subchannels << subchannel
-            end
+			protected
+			def add_subchannel subchannel
+				@subchannels << subchannel
+			end
 
 		end
 
 	end
 end
+
--- Kesh/Mumble/MessageHandler.class.rb
+++ Kesh/Mumble/MessageHandler.class.rb
@@ -5,41 +5,42 @@ module Kesh
 
 		class MessageHandler
 
-            def initialize client
-                client.register_text_handler "!find", method(:on_find)
-                client.register_text_handler "!goto", method(:on_goto)
-                client.register_text_handler "!test", method(:test)
-            end
+			def initialize client
+				client.register_text_handler "!find", method(:on_find)
+				client.register_text_handler "!goto", method(:on_goto)
+				client.register_text_handler "!test", method(:test)
+			end
 
-            private
-            def on_find client, message
-                text = message.message
+			private
+			def on_find client, message
+				text = message.message
 
-                nick = text[6..-1]
-                user = client.find_user nick
-                if user
-                    client.send_user_message message.actor, "User '#{user.name}' is in Channel '#{user.channel.path}'"
-                else
-                    client.send_user_message message.actor, "There is no user '#{nick}' on the Server"
-                end
-            end
+				nick = text[6..-1]
+				user = client.find_user nick
+				if user
+					client.send_user_message message.actor, "User '#{user.name}' is in Channel '#{user.channel.path}'"
+				else
+					client.send_user_message message.actor, "There is no user '#{nick}' on the Server"
+				end
+			end
 
-            def on_goto client, message
-                text = message.message
+			def on_goto client, message
+				text = message.message
 
-                nick = text[6..-1]
-                target = client.find_user nick
-                source = client.find_user message.actor
-                client.move_user source, target.channel
-            end
+				nick = text[6..-1]
+				target = client.find_user nick
+				source = client.find_user message.actor
+				client.move_user source, target.channel
+			end
 
-            def test client, message
-                client.channels.each do |id, ch|
-                    client.send_acl id
-                end
-            end
+			def test client, message
+				client.channels.each do |id, ch|
+					client.send_acl id
+				end
+			end
 
 		end
 
 	end
 end
+
