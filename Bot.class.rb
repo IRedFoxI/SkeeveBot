94fb7ab84f5e4adc54af3189ea839d1b253aa04a
Pim Messelink <pimmesselink@gmail.com>
2013-05-07 09:35:02 +0100

    Remove commented out old code and comment audio stuff

--- Bot.class.rb
+++ Bot.class.rb
@@ -80,80 +80,25 @@ class Bot
 
 		end
 
-		# return if !client.channel
-
-		# client.channel.localusers.each do |u|
-		# 	if u.session == client.user.session
-		# 		next # this is the master
-		# 	end
-
-		# 	if !@slave_by_host[client] or @slave_by_host[client][u.session]
-		# 		next # this is a slave from another server
-		# 	end
-
-		# 	if !@slave_by_user[client] or @slave_by_user[client][u.session]
-		# 		update_slaves(client, u)
-		# 		next # we have already slaves for this one
-		# 	end
-
-		# 	# new user
-		# 	@masters.each do |master, config|
-		# 		next if master == client # thats the current server
-		# 		host = @masters[client][:host]
-		# 		slave = make_slave Kesh::Mumble::MumbleClient.new(config[:host], config[:port], "#{u.name}@#{host}", @options)
-		# 		@slave_by_user[client][u.session] = [] if  !@slave_by_user[client][u.session]
-		# 		@slave_by_user[client][u.session] << slave
-		# 		@server_by_client[slave] = master
-		# 	end
-		# end
-
-		# #is a user missing? -> disconnect all slaves
-		# @slave_by_user[client].each do |session, slaves|
-		# 	is_in_channel = false
-		# 	client.channel.localusers.each do |u|
-		# 		is_in_channel = true if (session == u.session)
-		# 	end
-		# 	if !is_in_channel
-		# 		slaves.each do |slave|
-		# 			slave.disconnect
-		# 			remove_slave(slave, client)
-		# 		end
-		# 	end
-		# end
 	end
 
-	def on_audio client, message
-		packet = message.packet.bytes.to_a
+	# def on_audio client, message
+	# 	packet = message.packet.bytes.to_a
 
-		index = 0
-		tt = Kesh::Mumble::Tools.decode_type_target( packet[ index ] )
+	# 	index = 0
+	# 	tt = Kesh::Mumble::Tools.decode_type_target( packet[ index ] )
 
-		index = 1
-		vi1 = Kesh::Mumble::Tools.decode_varint packet, index
-		index = vi1[ :new_index ]
-		session = vi1[ :result ]
+	# 	index = 1
+	# 	vi1 = Kesh::Mumble::Tools.decode_varint packet, index
+	# 	index = vi1[ :new_index ]
+	# 	session = vi1[ :result ]
 
-		vi2 = Kesh::Mumble::Tools.decode_varint packet, index
-		index = vi2[ :new_index ]
-		sequence = vi2[ :result ]
+	# 	vi2 = Kesh::Mumble::Tools.decode_varint packet, index
+	# 	index = vi2[ :new_index ]
+	# 	sequence = vi2[ :result ]
 
-		data = packet[ index..-1 ]
-
-		# slaves = @slave_by_user[client][session]
-
-		# #is from real user?
-		# return if !slaves
-
-		# codec_type = tt[:type]
-
-		# slaves.each do |slave|
-		# 	if (codec_type == 0) or (codec_type == 3)
-		# 		tt[:type] = (client.alpha == slave.alpha) ? codec_type : 3 - codec_type
-		# 	end
-		# 	repackaged = Kesh::Mumble::Tools.encode_type_target(tt) + Kesh::Mumble::Tools.encode_varint(sequence) + data
-		# 	slave.send_udp_tunnel repackaged
-		# end
-	end
+	# 	data = packet[ index..-1 ]
+	# end
 
 	def run servers
 		servers.each do |server|
@@ -166,7 +111,7 @@ class Bot
 			client.register_handler :ServerSync, method( :on_connected )
 			client.register_handler :UserState, method( :on_users_changed )
 			client.register_handler :UserRemove, method( :on_users_changed )
-			client.register_handler :UDPTunnel, method( :on_audio )
+			# client.register_handler :UDPTunnel, method( :on_audio )
 			client.register_text_handler "!help", method( :cmd_help )
 			client.register_text_handler "!find", method( :cmd_find )
 			client.register_text_handler "!goto", method( :cmd_goto )
